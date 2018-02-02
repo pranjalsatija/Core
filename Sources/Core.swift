290e8535013eed4663648ec2db8e7874e76c358b
Pranjal Satija <pranjalsatija@users.noreply.github.com>
2018-02-02 16:27:30 -0600

    Add Instabug

--- Sources/Core.swift
+++ Sources/Core.swift
@@ -6,7 +6,9 @@
 //  Copyright © 2018 Pranjal Satija. All rights reserved.
 //
 
-public func initialize(withAppID appID: String, serverURL: String) {
+import Instabug
+
+public func initialize(withAppID appID: String, serverURL: String, instabugToken: String) {
     let config = ParseClientConfiguration {(config) in
         config.applicationId = appID
         config.server = serverURL
@@ -23,10 +25,19 @@ public func initialize(withAppID appID: String, serverURL: String) {
     Share.registerSubclass()
     View.registerSubclass()
     Visit.registerSubclass()
+
+    Instabug.start(withToken: instabugToken, invocationEvent: .shake)
+    Instabug.setIntroMessageEnabled(false)
+    Instabug.setPrimaryColor(UIColor(red: 61 / 255, green: 167 / 255, blue: 255 / 255, alpha: 1))
+    Instabug.setPromptOptionsEnabledWithBug(true, feedback: true, chat: false)
 }
 
 public func clearFileCaches() {
     PFFile.clearAllCachedDataInBackground()
+}
+
+public func provideFeedback() {
+    Instabug.invoke()
 }
 
 public func registerForNotifications(withToken token: Data) {
